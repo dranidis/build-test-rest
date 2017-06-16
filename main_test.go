65ed7c14b673a5271d080c89535a6dbcf530b4a9
dranidis <dranidis@gmail.com>
2017-06-16 22:47:53 +0300

    First test failed

---
+++ main_test.go
@@ -0,0 +1,74 @@
+package main_test
+
+import (
+	"os"
+	"testing"
+	"log"
+	"net/http"
+	"net/http/httptest"
+
+	"github.com/dranidis/build-test-rest"
+)
+
+var a main.App
+
+func TestMain(m *testing.M) {
+	a = main.App{}
+	a.Initialize(
+		os.Getenv("APP_DB_USERNAME"),
+		os.Getenv("APP_DB_PASSWORD"),
+		os.Getenv("APP_DB_NAME"))
+
+	ensureTableExists()
+
+	code := m.Run()
+
+	clearTable()
+
+	os.Exit(code)
+}
+
+func ensureTableExists() {
+	if _, err := a.DB.Exec(tableCreationQuery); err != nil {
+		log.Fatal(err)
+	}
+}
+
+const tableCreationQuery = `CREATE TABLE IF NOT EXISTS products
+(
+id SERIAL,
+name TEXT NOT NULL,
+price NUMERIC(10,2) NOT NULL DEFAULT 0.00,
+CONSTRAINT products_pkey PRIMARY KEY (id)
+)`
+
+func clearTable() {
+	a.DB.Exec("DELETE FROM products")
+	a.DB.Exec("ALTER SEQUENCE products_id_seq RESTART WITH 1")
+}
+
+func TestEmptyTable(t *testing.T) {
+	clearTable()
+
+	req, _ := http.NewRequest("GET", "/products", nil)
+	response := executeRequest(req)
+
+	checkResponseCode(t, http.StatusOK, response.Code)
+
+	if body := response.Body.String(); body != "[]" {
+		t.Errorf("Expected an empty array. Got %s", body)
+	}
+}
+
+func executeRequest(req *http.Request) *httptest.ResponseRecorder {
+	rr := httptest.NewRecorder()
+	a.Router.ServeHTTP(rr, req)
+
+	return rr
+}
+
+func checkResponseCode(t *testing.T, expected, actual int) {
+	if expected != actual {
+		t.Errorf("Expected response code %d. Got %d\n", expected, actual)
+	}
+}
